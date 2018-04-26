399d2751c09db12fe53d50f946a1a59d031d2b12
Cora Quezada <corafenice1013@gmail.com>
2018-04-25 23:28:42 -0400

    got links to linkedin & github

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,13 +7,32 @@ class Landing extends Component {
       <div style={{width: '100', margin: 'auto'}}>
         <Grid className="landing-grid">
           <Cell col={12}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWMRSfT_Bv_we9gCRBTgSBUthZi9V9jZ3uib8KE7cg20fPwrP0HQ" alt='profile picture' className='profile-pic'
-            />
-          </Cell>
-        </Grid>
-      </div>
-    )
-  }
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWMRSfT_Bv_we9gCRBTgSBUthZi9V9jZ3uib8KE7cg20fPwrP0HQ" alt='profile pic' className='profile-pic'
+          />
+          <div className="banner-text">
+            <h1>Full Stack Web Developer</h1>
+
+            <hr/>
+
+            <p>HTML/CSS | Ruby | Ruby Rails | JavaScript | React.js</p>
+            <div className="social-links">
+              {/* LinkedIn */}
+              <a href="http://google.com" target="_blank" rel="noopener noreferrer" >
+              <i className="fa fa-linkedin-square" aria-hidden="true" />
+            </a>
+            {/* GitHub */}
+            <a href="http://google.com"  target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-github-square" aria-hidden="true" />
+          </a>
+
+          </div>
+
+        </div>
+      </Cell>
+    </Grid>
+  </div>
+)
+}
 }
 
 export default Landing;
