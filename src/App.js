5d9d4f448ef906a45a7a3f01c800dbce669d5d8b
Cora Quezada <corafenice1013@gmail.com>
2018-04-25 22:49:43 -0400

    updated with routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer,Content } from 'react-mdl';
-
+import Main from './components/main'
 import './App.css';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header title="Title" scroll>
+        <Header className="header-color" title="Title" scroll>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
             <div className="page-content" />
+            <Main></Main>
         </Content>
     </Layout>
 </div>
