f44438af8ed69e22cd2eeba8a8b0aee350f85367
Cora Quezada <corafenice1013@gmail.com>
2018-04-27 21:09:06 -0400

    added projects to projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, CardMenu, Butoon, IconButton} from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, CardMenu, Button, IconButton} from 'react-mdl';
 
 class Project extends Component {
   constructor(props) {
@@ -9,24 +9,121 @@ class Project extends Component {
   toggleCategories() {
     if (this.state.activeTab === 0){
       return(
+        <div className="project-grid">
+          {/*Project #1 */}
         <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-          <CardTitle style={{color: '#fff', height: '176px', background:'url() center /cover'}}</CardTitle>
-
-
-
+          <CardTitle style={{color: '#fff', height: '176px', background:'url() center /cover'}} >React Project #1</CardTitle>
+          <CardText>
+            What ever I want to describe project
+          </CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+            <Button colored>Demo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name='share'/>
+          </CardMenu>
         </Card>
+        {/*Project #2 */}
+      <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+        <CardTitle style={{color: '#fff', height: '176px', background:'url() center /cover'}} >React Project #2</CardTitle>
+        <CardText>
+          What ever I want to describe project
+        </CardText>
+        <CardActions border>
+          <Button colored>GitHub</Button>
+          <Button colored>Demo</Button>
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+          <IconButton name='share'/>
+        </CardMenu>
+      </Card>
+      {/*Project #3 */}
+    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+      <CardTitle style={{color: '#fff', height: '176px', background:'url() center /cover'}} >React Project #3</CardTitle>
+      <CardText>
+        What ever I want to describe project
+      </CardText>
+      <CardActions border>
+        <Button colored>GitHub</Button>
+        <Button colored>Demo</Button>
+      </CardActions>
+      <CardMenu style={{color: '#fff'}}>
+        <IconButton name='share'/>
+      </CardMenu>
+    </Card>
+  </div>
       )
     }  else if(this.state.activeTab === 1) {
       return (
-        <div><h1> This is JavaScript</h1></div>
+        <div className="project-grid">
+          {/*Project #1 */}
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background:'url() center /cover'}} >React Project #1</CardTitle>
+          <CardText>
+            What ever I want to describe project
+          </CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+            <Button colored>Demo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name='share'/>
+          </CardMenu>
+        </Card>
+  </div>
       )
     } else if(this.state.activeTab === 2) {
       return (
-        <div><h1>This is Ruby Rails</h1></div>
+        <div className="project-grid">
+          {/*Project #1 */}
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background:'url() center /cover'}} >React Project #1</CardTitle>
+          <CardText>
+            What ever I want to describe project
+          </CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+            <Button colored>Demo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name='share'/>
+          </CardMenu>
+        </Card>
+        {/*Project #2 */}
+      <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+        <CardTitle style={{color: '#fff', height: '176px', background:'url() center /cover'}} >React Project #2</CardTitle>
+        <CardText>
+          What ever I want to describe project
+        </CardText>
+        <CardActions border>
+          <Button colored>GitHub</Button>
+          <Button colored>Demo</Button>
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+          <IconButton name='share'/>
+        </CardMenu>
+      </Card>
+  </div>
       )
-    } else if(this.state.activeTab) === 3) {
-      return(
-        <div><h1>???????????????</h1></div>
+    } else if(this.state.activeTab === 3) {
+      return (
+        <div className="project-grid">
+          {/*Project #1 */}
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background:'url() center /cover'}} >React Project #1</CardTitle>
+          <CardText>
+            What ever I want to describe project
+          </CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+            <Button colored>Demo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name='share'/>
+          </CardMenu>
+        </Card>
+  </div>
       )
     }
   }
@@ -41,13 +138,12 @@ class Project extends Component {
           <Tab>??????</Tab>
         </Tabs>
 
-        <section className="projects-grid">
-          <Grid className="projects-grid">
+
+          <Grid>
             <Cell col={12}>
               <div className="content">{this.toggleCategories()}</div>
             </Cell>
           </Grid>
-        </section>
       </div>
     )
   }
